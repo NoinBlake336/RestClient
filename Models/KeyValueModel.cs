94d19cd85606c5e02d9263b9e849c05ff4545794
NoinBlake336 <josechocobar336@gmail.com>
2024-09-18 01:18:03 -0300

    add parameters requests

---
+++ Models/KeyValueModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestClient.Models
+{
+    public class KeyValueModel
+    {
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+    }
+}
